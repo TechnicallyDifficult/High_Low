b17ef08dba86b6cf524445428d9d0b6a0d862f56
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-03-14 13:07:44 -0500

    fixes bug where guesses displayed after winning would be 1 less than the correct amount; game can now take arguments when called from command line and calculates the allowed number of guesses based on the provided min and max values

--- guess_the_number.php
+++ guess_the_number.php
@@ -1,31 +1,37 @@
 <?php
 
-fwrite(STDOUT, "I'M THINKING..." . PHP_EOL);
+fwrite(STDOUT, PHP_EOL . "I'M THINKING..." . PHP_EOL);
 
-$number = mt_rand(1, 100);
+$min = $argc > 1 ? (int) $argv[1] : 1;
+$max = $argc > 2 ? (int) $argv[2] : $min + 99;
+$number = mt_rand($min, $max);
+$maxGuesses = (int) log(($max - $min + 1), 2);
 
-fwrite(STDOUT, 'OK!' . PHP_EOL);
-fwrite(STDOUT, 'A NUMBER BETWEEN 1 AND 100.' . PHP_EOL);
-for ($guessesTaken = 0; $guessesTaken < 6; $guessesTaken++) {
+fwrite(STDOUT, 'OK!' . PHP_EOL . PHP_EOL);
+fwrite(STDOUT, "A NUMBER BETWEEN $min AND $max" . PHP_EOL);
+fwrite(STDOUT, "$maxGuesses GUESSES" . PHP_EOL . PHP_EOL);
+
+for ($guessesTaken = 0; $guessesTaken < $maxGuesses; $guessesTaken++) {
 	fwrite(STDOUT, 'GUESS?' . PHP_EOL);
 
 	$response = trim(fgets(STDIN));
 	
 	if ((int) $response == $number) {
-		fwrite(STDOUT, 'GOOD GUESS!' . PHP_EOL);
+		fwrite(STDOUT, PHP_EOL . 'GOOD GUESS!' . PHP_EOL);
 		break;
-	} elseif ($response < $number and $guessesTaken < 5) {
-		fwrite(STDOUT, 'HIGHER' . PHP_EOL);
-	} elseif ($response > $number and $guessesTaken < 5) {
-		fwrite(STDOUT, 'LOWER' . PHP_EOL);
+	} elseif ($response < $number and $guessesTaken < $maxGuesses) {
+		fwrite(STDOUT, PHP_EOL . 'HIGHER' . PHP_EOL);
+	} elseif ($response > $number and $guessesTaken < $maxGuesses) {
+		fwrite(STDOUT, PHP_EOL . 'LOWER' . PHP_EOL);
 	}
 }
 
-if ($guessesTaken < 6) {
-	fwrite(STDOUT, "TOOK YOU $guessesTaken GUESSES." . PHP_EOL);
+if ($guessesTaken < $maxGuesses) {
+	$guessesTaken++;
+	fwrite(STDOUT, "TOOK YOU $guessesTaken GUESSES" . PHP_EOL);
 } else {
-	fwrite(STDOUT, 'SORRY. NO MORE GUESSES.' . PHP_EOL);
-	fwrite(STDOUT, "NUMBER WAS $number." . PHP_EOL);
+	fwrite(STDOUT, 'SORRY. NO MORE GUESSES' . PHP_EOL);
+	fwrite(STDOUT, "NUMBER WAS $number" . PHP_EOL);
 }
 
 exit(0);
